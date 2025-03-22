ae1bf54bba0fefa7f169d8d22dce4647de53c588
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-03-22 19:18:57 -0300

    Implementado metódo add();

--- src/one/digitalinovation/ListaDuplamenteEncadeada.java
+++ src/one/digitalinovation/ListaDuplamenteEncadeada.java
@@ -14,6 +14,19 @@ public class ListaDuplamenteEncadeada<T> {
     public T get(int index){
         return this.getNo(index).getConteudo();
     }
+    public void add(T elemento){ //metódo que adiciona ao fim da lista.
+        NoDuplo<T> novoNo = new NoDuplo<T>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if (primeiroNo == null) {
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
     private NoDuplo<T> getNo(int index){ //metodo de uso interno da classe.
         NoDuplo<T> noAuxiliar = primeiroNo;
         for(int i = 0; (i < index) && (noAuxiliar != null); i++){
